5ec3b907bb236a7b7f7f531477174cc089389702
lhwhit <lhwhit@users.noreply.github.com>
2018-10-23 19:24:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<video width="320" height="240" controls="controls" autoplay="autoplay">
+<source src="01-01.mp4" type="video/mp4" />
+</video>
+
+<?php
+echo "操作系统课程介绍";
+?>
+
+</body>
+</html>
